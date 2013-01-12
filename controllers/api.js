09372e30be058cc7daa454d546f82c34fe9c50eb
William Turner <willtur.will@gmail.com>
2013-01-12 15:02:11 -0500

    criss

--- controllers/api.js
+++ controllers/api.js
@@ -2,8 +2,9 @@ module.exports = function(app, db, prefix) {
   app.get(prefix + '/api', function(req, res) {
     res.json(db.search(req.param('q')));
   });
-
+/*
   app.get(prefix + '/api/:id', function(req, res) {
     res.json(db.documents[req.params.id]);
   });
+*/
 };
