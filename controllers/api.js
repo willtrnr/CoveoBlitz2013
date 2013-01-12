eddf9c5409d274edb369130fe0d9e8ee4cb9e959
William Turner <willtur.will@gmail.com>
2013-01-12 13:42:27 -0500

    PAI mal ecrit

--- controllers/api.js
+++ controllers/api.js
@@ -2,4 +2,8 @@ module.exports = function(app, db, prefix) {
   app.get(prefix + '/api', function(req, res) {
     res.json(db.search(req.param('q')));
   });
+
+  app.get(prefix + '/api/:id', function(req, res) {
+    res.json(db.documents[req.param('q')]);
+  });
 };
