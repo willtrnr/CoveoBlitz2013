080bfffd17df88cf99146b0688a62eafff2e609f
Francis Marineau <frank.marineau@gmail.com>
2013-01-12 16:36:30 -0500

    asdklj

--- datastore/index.js
+++ datastore/index.js
@@ -252,10 +252,10 @@ module.exports = function(config) {
   this.getUIArtists = function(artists) {
     var str = "";
     if (artists.length > 0)
-      str.push(artists[0]);
+      str += artists[0];
     for (var i in artists) {
       if (i > 0)
-        str.push(', ' + artists[i]);
+        str += ', ' + artists[i];
     }
     return str;
   };
